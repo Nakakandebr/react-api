fdbd69e39d58d4b3b064b95602a6c20d8df15a74
Nakakandebr <bridgetnakakandea@gmail.com>
2023-07-04 21:55:31 +0300

    commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Login from './Login';
+import Products from './Login/Products';
+
+function App() {
+  return (
+    <div>
+      <Login/>
+      <Products/>
+    </div>
+  );
+}
+
+export default App;
